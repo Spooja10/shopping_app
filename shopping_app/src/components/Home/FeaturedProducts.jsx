0e891d195c39944546eb21393ce54bb416e30c8d
Spooja10 <113345889+Spooja10@users.noreply.github.com>
2025-02-10 11:00:42 +0530

    Update FeaturedProducts.jsx

--- shopping_app/src/components/Home/FeaturedProducts.jsx
+++ shopping_app/src/components/Home/FeaturedProducts.jsx
@@ -5,8 +5,6 @@ import Productcard from "../Products/Productcard";
 import useData from './../../hooks/useData';
 import ProductCardSkeleton from "../Products/ProductCardSkeleton";
 
-
-
 const FeaturedProducts = () => {
   const {data, error1, isLoading} = useData("/products/featured");
   const skeletons = [1, 2, 3]
@@ -17,11 +15,6 @@ const FeaturedProducts = () => {
       <div className="align_center featured_products_list">
         <Productcard />
         
-        {/* {error1 && <em className='form_error'>{error1}</em>}
-        { data.map(product => (
-          <Productcard key={product.id} product={product} />
-        ))}
-        {isLoading && skeletons.map(n => <ProductCardSkeleton key={n}/>) }   */}
       </div>
     </section>
   );
