1ec4849f52701f4e2587aeeb9c0a7e32d7da3bdb
Spooja10 <113345889+Spooja10@users.noreply.github.com>
2025-02-10 11:01:41 +0530

    Update LinkWithIcon.jsx

--- shopping_app/src/components/Navbar/LinkWithIcon.jsx
+++ shopping_app/src/components/Navbar/LinkWithIcon.jsx
@@ -1,20 +1,3 @@
-// import React from 'react'
-
-// import './LinkWithIcon.css'
-// import { NavLink } from 'react-router-dom';
-
-// const LinkWithIcon = ({ title, link, emoji, sidebar}) => {
-//   return (
-//      <NavLink to={link} className={sidebar ? 'align_center sidebar_link' : 'align_center'}>{title} 
-//      <img src={emoji} alt="" className='link_emoji'/>
-//      </NavLink>
-
-    
-// )
-// }
-
-// export default LinkWithIcon;
-
 import React from 'react';
 import { Link } from 'react-router-dom';
 
